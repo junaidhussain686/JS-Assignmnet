fa3ecffbf063b9f1a32340d3df44e31b19ef5840
Junaid Hussain <junaidhussain686@gmail.com>
2024-09-21 15:44:03 +0500

    chap01 to chap05
    assignment is done

---
+++ CHP01 To 05 Assignment/app.js
@@ -0,0 +1,224 @@
+// ~Chapter No.01~
+
+//Question No-01
+// alert('Hello')
+
+//Question No-02
+// alert('Error!Please Enter a Valid Password')
+
+// Question No-03
+// alert('Welcome To JS Land...\nHappy Coding!')
+
+//Question No-04
+// alert('Welcome To JS Land...')
+// alert('Happy Coding!\nPrevent this page from creating additional dialogs')
+
+//Question No-05
+// console.log("Hello... I can run JS through my web browser's console")
+
+// ~Chapter No.02~
+
+//Question No-01
+// var userName ;
+
+//Question No-02
+// var myName ='Muhammad Junaid Hussain'
+// alert(myName)
+
+// Question No-03
+// var message = 'Hello World'
+// alert(message);
+
+//Question No-04
+// var studentName;
+// studentName='Ali';
+// alert(studentName)
+// var age='15 Years Old';
+// alert(age)
+// var passion='Certified Mobile Application Development';
+// alert(passion)
+
+//Question No-05
+// var pizzaPattern="PIZZA\nPIZZ\nPIZ\nPI\nP";
+// alert(pizzaPattern)
+
+//Question No-06
+// var email='junaidhussain0331@gmail.com'
+// alert("My email address is "+email)
+
+//Question No-07
+// var book =' A smarter way to learn JavaScript'
+// alert("I am trying to learn from the Book"+book)
+
+//Question No-08
+// document.write("Yah!I can write HTML content through JavaScript")
+
+//Question No-09
+// var animation="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+// alert(animation)
+// document.write(animation)
+
+// ~Chapter No.03~
+
+//Question No-01
+// var age =20;
+// alert(age)
+
+//Question No-02
+// var trackVisitor =12;
+// alert("You Have visited this site "+trackVisitor+" times")
+
+//Question No-03
+// var birthYear=2004;
+// document.write("My birth year is "+birthYear +"<br>"+"Data type of my declared Variable is Number")
+
+//Question No-04
+// var visitorName='John Doe'
+// var productTitle='T-Shirts'
+// var orderOfvisitor=5
+
+// document.write(visitorName+" ordered "+orderOfvisitor + " " + productTitle +"(s) on XYZ Clothing store")
+
+// ~Chapter No.04~
+
+//Question No-01
+
+// var name='Hassan' , age=21 , fatherName='Farhan'
+
+//Question No-02
+// Legal Variable Name
+// var Name;
+// var userName;
+// var rollNo;
+// var emailAddress;
+// var phoneNumber;
+
+// Illegal Variable Name
+// var 2ndName;
+// var user-name;
+// var @RollNo;
+// var email address;
+// var &let;
+
+//Question No-03
+// document.write("<h1>Rules for naming JS variables</h1>");
+// document.write("<p>Variable names can only contain numbers, $ and _. For example: $my_1stVariable</p>");
+// document.write("<p>Variables must begin with a letter, $, or _. For example: $name, _name, or name</p>");
+// document.write("<p>Variable names are case sensitive</p>");
+// document.write("<p>Variable names should not be JS keywords</p>");
+
+// ~Chapter No.05~
+ 
+//Question No-01
+//  var a=+prompt("Please Enter the First Number")
+// var b=+prompt("Please Enter the Second Number")
+// document.write('The Sum of ' +a+ ' and ' +b+ ' is ' + Number(a+b) )
+
+ //Question No-02
+//  var a=prompt("Please Enter the First Number")
+// var b=prompt("Please Enter the Second Number")
+// document.write('The Sum of ' +a+ ' and ' +b+ ' is ' + Number(a-b) )
+
+// var a=prompt("Please Enter the First Number")
+// var b=prompt("Please Enter the Second Number")
+// document.write('The Sum of ' +a+ ' and ' +b+ ' is ' + Number(a*b) )
+
+// var a=prompt("Please Enter the First Number")
+// var b=prompt("Please Enter the Second Number")
+// document.write('The Sum of ' +a+ ' and ' +b+ ' is ' + Number(a/b) )
+
+// var a=prompt("Please Enter the First Number")
+// var b=prompt("Please Enter the Second Number")
+// document.write('The Sum of ' +a+ ' and ' +b+ ' is ' + Number(a%b) )
+
+//Question No-03
+// var a
+// document.write("Value After Declaration is "+a + "<br>") 
+// a=5
+// document.write("Initial Value: "+a+"<br>")
+// a++
+// document.write("Value Of Increment is: "+a+"<br>")
+// a= a+7
+// document.write("Value after addition is: "+a+"<br>")
+// a--
+// document.write("Value Of decrement is: "+a+"<br>")
+// a=a%3
+// document.write("The Remainder is: "+a+"<br>")
+
+//Question No-04
+// var a=600
+// document.write("Total cost to buy 5 tickets to a movie is "+a*5+"PKR")
+
+//Question No-06
+// var a=25
+// f=(a*9/5)+32
+// document.write ("25<sup>0</sup>C is " +f+"F"+"<br>") 
+// var b =70
+// c=(b-32)*5/9
+// document.write ("70<sup>0</sup>F is " +c+"C")
+
+//Question No-07
+// var a=650
+// document.write("Price of item 1 is "+a+"<br>")
+// var q1=3
+// document.write("Quantity of item 1 is "+q1+"<br>")
+// var b=100
+// document.write("Price of item 2 is "+b+"<br>")
+// var q2=7
+// document.write("Quantity of item 2 is "+q2+"<br>")
+// var s=100
+// document.write("Shipping charges is "+s+"<br>"+"<br>")
+// var result=(a*q1)+(b*q2)+s
+// document.write("Total cost of your Order is "+result)
+
+//Question No-08
+// var totalMarks=850
+// var marksObtained=769
+
+// var result=(marksObtained/totalMarks)*100
+// document.write("<h1> Marksheet </h1>")
+// document.write("Total Marks : "+totalMarks+"<br>"+"Marks Obtained : "+marksObtained+"<br>"+"Percentage: "+result+"%")
+
+//Question No-09
+//  var usDollars=104.80
+//  var saudiRiyal=28
+
+//  var usTopkr=usDollars*10
+//  var riyalTopkr=saudiRiyal*25
+
+//  var result=usTopkr+riyalTopkr
+//  document.write("Total Currency in PKR is : "+result)
+
+//Question No-10
+
+// var num=10
+
+// document.write("By Adding The Number is : " +Number(num+5) +"<br>"+"By Multiplication The Number is : "+num*10+"<br>"+"By Division The Number is : "+num/2)
+
+//Question No-11
+
+// var currentYear=2024
+// var birthYear=2004
+
+// var age=currentYear-birthYear
+
+// document.write("<h1> Age Calculator </h1>"+"<br>"+"Current Year : "+currentYear+"<br>"+"Birth Year : "+birthYear+"<br>"+"Your Age is :"+age)
+
+//Question No-12
+// var radius =20 ;
+
+// var circumferenceOFcircle=2*3.142*radius
+
+// var area=3.142*radius*radius
+
+// document.write("<h1> The Geometrizer </h1>"+"<br>"+"Radius of a circle: "+radius+"<br>"+"The cicumference of a Circle is : "+circumferenceOFcircle+"<br>"+"The area is :"+area)
+
+//Question No-13
+// var snack='lays'    
+// var currentAge=20
+// var maximumAge=65
+// var estimatedAmount=4   
+// var lifetimesupply=(maximumAge-currentAge)*365*estimatedAmount
+
+// document.write("<h1> The Lifetime Supply Calculator  </h1>"+"<br>"+"Favourite Snack: "+snack+"<br>"+"Current Age : "+currentAge+"<br>"+"Estimated Maximum Age :"+maximumAge+"<br>"+"Amount of snacks per day: "+estimatedAmount+"<br>"+"You will need "+lifetimesupply+" lays to last you until the ripe old age of "+maximumAge)
+
