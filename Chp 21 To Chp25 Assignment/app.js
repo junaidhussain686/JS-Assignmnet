211c193dc7c5c6397ade7e5c52c672ba7187b6f7
Junaid Hussain <junaidhussain686@gmail.com>
2024-12-15 01:06:32 +0500

    .
    updated

---
+++ Chp 21 To Chp25 Assignment/app.js
@@ -0,0 +1,41 @@
+// ~Chapter No.21~ To ~Chapter No.25~
+
+//Question No-01
+
+// var firstName=prompt("Enter your First Name ")
+// var lastName=prompt("Enter your Last Name")
+
+// var fullName=firstName+" "+lastName
+// console.log(fullName)
+
+//Question No-02
+
+// var phoneModel=prompt("Enter Your Favorite Phone Model")
+// console.log(phoneModel)
+// console.log("Length of String : "+phoneModel.length)
+
+//Question No-03
+
+var index ='Pakistani'
+
+console.log("String: "+index)   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
